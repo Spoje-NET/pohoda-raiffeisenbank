2aed7862042338b343b947bb6dafc7f92f2be813
CyberVitexus <info@vitexsoftware.cz>
2024-07-03 09:31:21 +0200

    setup hotfix

--- src/pohoda-raiffeisenbank-setup.php
+++ src/pohoda-raiffeisenbank-setup.php
@@ -13,22 +13,25 @@ require_once('../vendor/autoload.php');
 /**
  * Get List of bank accounts and import it into Pohoda
  */
-\Ease\Shared::init(['POHODA_URL', 'POHODA_LOGIN', 'POHODA_PASSWORD', 'POHODA_COMPANY', 'CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID'], isset($argv[1]) ? $argv[1] : '../.env');
+\Ease\Shared::init(['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD', 'POHODA_ICO', 'CERT_FILE', 'CERT_PASS', 'XIBMCLIENTID'], isset($argv[1]) ? $argv[1] : '../.env');
 $apiInstance = new \VitexSoftware\Raiffeisenbank\PremiumAPI\GetAccountsApi();
 $x_request_id = time(); // string | Unique request id provided by consumer application for reference and auditing.
 
 Transactor::checkCertificatePresence(\Ease\Shared::cfg('CERT_FILE'));
+
+exit(0); //TODO
+
 try {
     $result = $apiInstance->getAccounts($x_request_id);
     if (array_key_exists('accounts', $result)) {
-        $banker = new \Pohoda\RW(null, ['evidence' => 'bankovni-ucet']);
+        $banker = new \SpojeNet\PohodaSQL\DOC();
         if (\Ease\Shared::cfg('APP_DEBUG')) {
             $banker->logBanner($apiInstance->getConfig()->getUserAgent());
         }
         $currentAccounts = $banker->getColumnsFromPohoda(['id', 'kod', 'nazev', 'iban', 'bic', 'nazBanky', 'poznam'], ['limit' => 0], 'iban');
         foreach ($result['accounts'] as $account) {
             if (array_key_exists($account->iban, $currentAccounts)) {
-                $banker->addStatusMessage(sprintf('Account %s already exists in flexibee as %s', $account->friendlyName, $currentAccounts[$account->iban]['kod']));
+                $banker->addStatusMessage(sprintf('Account %s already exists in pohoda as %s', $account->friendlyName, $currentAccounts[$account->iban]['kod']));
             } else {
                 $banker->dataReset();
                 $banker->setDataValue('kod', 'RB' . $account->accountId);
@@ -41,8 +44,8 @@ try {
                 $banker->setDataValue('bic', $account->bankBicCode);
                 $saved = $banker->sync();
                 $banker->addStatusMessage(
-                    sprintf('Account %s registered in flexibee as %s', $account->friendlyName, $banker->getRecordCode()),
-                    ($saved ? 'success' : 'error')
+                        sprintf('Account %s registered in flexibee as %s', $account->friendlyName, $banker->getRecordCode()),
+                        ($saved ? 'success' : 'error')
                 );
             }
         }
