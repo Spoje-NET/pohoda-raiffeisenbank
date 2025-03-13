64fa1775dd3786b7dbc36615baac6f8c40e76210
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-03-13 13:33:26 +0100

    cli configuration update for offline importer

--- src/pohoda-raiffeisenbank-offline-statement-importer.php
+++ src/pohoda-raiffeisenbank-offline-statement-importer.php
@@ -28,7 +28,10 @@ $destination = \array_key_exists('o', $options) ? $options['o'] : (\array_key_ex
 /**
  * Get today's Statements list.
  */
-\Ease\Shared::init(['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD', 'POHODA_ICO', 'POHODA_BANK_IDS', 'ACCOUNT_NUMBER'], \array_key_exists('environment', $options) ? $options['environment'] : '../.env');
+\Ease\Shared::init(
+    ['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD', 'POHODA_ICO', 'POHODA_BANK_IDS', 'ACCOUNT_NUMBER'],
+    \array_key_exists('environment', $options) ? $options['environment'] : (\array_key_exists('e', $options) ? $options['e'] : '../.env'),
+);
 $engine = new Statementor(Shared::cfg('ACCOUNT_NUMBER'));
 
 if (Shared::cfg('APP_DEBUG')) {
